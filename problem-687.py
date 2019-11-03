6d02464436987066a247d651d215d7a8f046970b
Mark M <otteromkram@gmail.com>
2019-11-03 15:31:18 -0500

    First commit

---
+++ problem-687.py
@@ -0,0 +1,27 @@
+
+"""
+Purpose: Project Euler problems
+Date created: 2019-11-03
+Contributor(s): Mark M.
+
+ID: 687
+Title: Shuffling Cards
+URI: https://projecteuler.net/problem=687
+
+Status: Incomplete
+
+Desc: 
+    A standard deck of 52 playing cards, which consists of thirteen ranks
+    (Ace, Two, ..., Ten, King, Queen and Jack) each in four suits (Clubs,
+    Diamonds, Hearts and Spades), is randomly shuffled.  Let us call a rank
+    perfect if no two cards of that same rank appear next to each other after
+    the shuffle.
+    
+    It can be seen that the expected number of ranks that are perfect after a
+    random shuffle equals 4324425 ≈ 10.1741176471.
+    
+    Find the probability that the number of perfect ranks is prime. Give your
+    answer rounded to 10 decimal places.
+"""
+
+
