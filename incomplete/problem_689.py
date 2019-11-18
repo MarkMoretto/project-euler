76c5b4fd695b684c1a78e7b4cf24327e2fae1a9d
Mark Moretto <MMorett1@hfhs.org>
2019-11-18 09:25:04 -0500

    Update problem_689.py

--- incomplete/problem_689.py
+++ incomplete/problem_689.py
@@ -19,9 +19,14 @@ Problem:
 
 """
 
+import re
+import typing as T
+Q = T.TypeVar('Q', str, float, int,)
+N = T.TypeVar('N', str, float,)
+s_vec = T.List[str]
+n_vec = T.List[N]
 
-BinaryNumber = 1001111
-Decimal value = (1*(2^6)) + (0*(2^5)) + (0*(2^4)) + (1*(2^3)) + (1*(2^2)) + (1*(2^1)) + (1*(2^0))
+
 
 def to_decimal(n: int) -> int:
     n_str: str = str(n)
@@ -29,4 +34,97 @@ def to_decimal(n: int) -> int:
         print(f'({v}*(2^{i}))')
 
 
-# def flost_to_bin():
+
+tst1 = 0.890625
+
+tmp = [i*2 for i in range(len(str(tst1)))]
+
+def eval_float(float_value: float) -> int:
+    if float_value > 1:
+        return 1
+    return  0
+
+
+def flost_to_bin(float_value: float, lst: list = None) -> int:
+    val = float_value
+    max_bits = len(str(float))
+
+    if lst is None:
+        lst = list()
+
+    while len(str(val)) > 2:
+        tmp = val * 2
+        print(tmp)
+        lst.append(str(eval_float(tmp)))
+        val = tmp % 1
+        print(val)
+
+    return (''.join(lst)[::-1])
+
+
+def make_hexadecimal_dict() -> T.Dict[str, N]:
+    abc_list: s_vec = ['a','b','c','d','e','f']
+    tmp_dict: T.Dict[int, str] = {i:str(i) for i in range(10)}
+    for i, v in enumerate(abc_list, start=10):
+        tmp_dict[i] = v
+    return {v:k + 0.0 for k,v in tmp_dict.items()}
+
+
+hex_dict = make_hexadecimal_dict()
+
+
+tst2 = float.hex(3740.0) # '0x1.d380000000000p+11'
+(1 + 13./16**1 + 3./16**2 + 8/16**3) * (2.0 ** 11)
+
+tst2 = float.hex(0.25) # '0x1.0000000000000p-2'
+
+pat1 = r"""
+    -?\d+               # sign and decimal (nehgative or nothing)
+    x(?P<d>\d+)         # x and subsequent decimal
+    \.(?P<f>\w+?\d+)    # fractional part
+    (?P<e>\+\d+|-\d+)   # exponent
+    """
+
+
+tst2 = float.hex(3740.0) # '0x1.d380000000000p+11' // Original "0x3.a7p10"
+pat1 = r"""(-|\+)?0x(\d+)\.(\w*?\d+)p(.\d+)"""
+p = re.compile(pat1)
+sign, integer, fraction, exponent = p.findall(tst2)[0] # Need to unpack list
+
+
+
+def enum(it: T.Iterable[Q], start: int = 0) -> T.Generator[int, Q]:
+    for element in it:
+        yield start, element
+        start += 1
+
+
+
+def tokenizer(it: T.Iterable[str]) -> T.Iterable[str]:
+    return [it[i] for i in range(len(it))]
+
+
+
+def hex_to_dec(hex_string: str) -> float:
+    output: str = ''
+
+    hex_pat: str = r"""(-|\+)?0x(\d+)\.(\w*?\d+)p(.\d+)"""
+    p = re.compile(hex_pat)
+    sign, integer, fraction, exponent = p.findall(hex_string)[0] # Need to unpack list
+
+    tokens = tokenizer(fraction)
+
+    eval_list = [f'{hex_dict[v]}/16**{i}' for i, v in enum(tokens, start=1) if v.isdigit() and int(v) > 0 or v.isalpha()]
+    
+    output = f'({str(integer)}' + ' + '
+    output += ' + '.join(eval_list)
+    output += ') * ' + str(f'(2**{exponent})')
+
+    eval(output)
+
+
+
+
+
+
+
