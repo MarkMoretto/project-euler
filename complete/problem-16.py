1ee277dcfb5e9eab09664793e5e1312a333e6c0b
Mark Moretto <otteromkram@gmail.com>
2020-02-01 08:45:17 -0500

    Update problem-16.py

--- complete/problem-16.py
+++ complete/problem-16.py
@@ -1,5 +1,19 @@
+"""
+Purpose: Project Euler problems
+Date created: 2020-02-01
 
+ID: 16
+Title: Power digit sum
+URI: https://projecteuler.net/problem=16
+Status: Complete
+Contributor(s): Mark Moretto
 
+Description:
+  2*15 = 32768 and the sum of its
+  digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+  What is the sum of the digits of the number 2^1000?
+"""
 
 base = 2
 expon = 1000
