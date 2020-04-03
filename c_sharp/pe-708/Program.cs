81f27451e11839c3efb491f8a60f95f3db15da0f
Mark M <otteromkram@gmail.com>
2020-04-03 18:20:45 -0400

    C-sharpin' through life!

--- c_sharp/pe-708/Program.cs
+++ c_sharp/pe-708/Program.cs
@@ -3,13 +3,28 @@
 namespace pe_708 {
     class Program {
         static void Main(string[] args) {
-            if (args.length == 1) {
+            if (args.Length > 0) {
                 Console.WriteLine($"Hello, {args[0]}!");
             } else {
                 Console.WriteLine("Hello, World!");    
             }
             
-            Console
+            Console.WriteLine("Fibonacci numbers 1 - 15:");
+            for (int i = 0; i < 15; i++) {
+                Console.WriteLine($"{i + 1}: {FibonacciNumbers(i)}");
+            }
+        }
+
+        static int FibonacciNumbers(int n) {
+            int a = 0;
+            int b = 1;
+            int tmp;
+            for (int i = 0; i < n; i++) {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+            return a;
         }
     }
 }
